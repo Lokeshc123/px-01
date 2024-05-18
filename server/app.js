c46712a2e0911cbd2ecda214bb5edfd47b5ab985
Lokeshc123 <lokeshchauhan629@gmail.com>
2024-05-18 13:07:48 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -18,8 +18,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    credentials: true,
   })
 );
 
